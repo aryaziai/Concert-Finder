496e69c95cfd836bfb77b8fa4d3e6ae74de75593
github username <github email address>
2019-11-12 16:26:41 -0800

    Joseph create ticket table

---
+++ db/migrate/20191112194053_ticket.rb
@@ -0,0 +1,9 @@
+class CreateTickets < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tickets do |t|
+      t.integer :seat
+      t.integer :customer_id
+      t.integer :concert_id
+    end
+  end
+end
